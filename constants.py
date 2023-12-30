2394fde5211289ab6faefd9550a9f8a215f77606
rachidapprendsacoder <choumikar@gmail.com>
2023-12-30 12:13:26 +0100

    d

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import time
 
-screen_size_x = 1900  # pour un fonctionnement du jeu optimal, cette valeur doit être un multiple de 13/19
+screen_size_x = 1900  # pour un fonctionnement du jeu optimal, cette valeur doit être 1900 ou plus
 screen_size_y = (13 / 19) * screen_size_x
 sq_size = 2 / 19 * screen_size_x
 path_size = 0.5 * sq_size
